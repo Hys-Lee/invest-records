141b47d104b7fbc3785c9a3721d278bfb991df73
Yunsoo <yunsoojoh@gmail.com>
2023-10-01 15:39:52 +0900

    feat: proxy setting done

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/stock': {
+        target: 'https://www.koreaexim.go.kr',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/stock/, ''),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+});
