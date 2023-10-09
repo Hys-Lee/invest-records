b9ee760f5bbdca3972980f24a460fcaf1b9fe32b
Hys-Lee <dltkdgus3275@gmail.com>
2023-10-09 10:12:03 +0900

    Feat: new UI branch & arrange folder structure
    UI for main summary tile and each stock tiles
    distributes files into api, components, pages
    components for elements of pages with reusability

---
+++ src/components/Record.tsx
@@ -0,0 +1,14 @@
+import { RecordType } from '.';
+
+export default function Record({ history }: any): React.ReactElement {
+  // 더 디테일한 정보는 클릭으로 봐야할 것 같은데.
+  return (
+    <div className="bg-gray-400 m-1">
+      <h1>{history.tic}</h1>
+      <h2>{history.date.toString()}</h2>
+      <h2>{history.purchasingExchangeRate}</h2>
+      <h2>{history.purchaseAmount}</h2>
+      <h3>{history.quantity}</h3>
+    </div>
+  );
+}
---
+++ src/components/Selection.tsx
@@ -0,0 +1,68 @@
+import { StockType } from './index';
+import { useState } from 'react';
+
+import Stock from './Stock';
+type SelectionProps = {
+  stocks: StockType[];
+  onStocks: any;
+};
+
+export default function Selection({ stocks, onStocks }: SelectionProps): React.ReactElement {
+  //   console.log(stocks); // 이거 왜 2번씩 불리는지 이해가 안감. -> strict mode 때문인 듯?
+  const [showing, useShowing] = useState<boolean>(false);
+
+  const allChecked = stocks.filter((s) => s.selected === false).length === 0;
+  const selected = stocks.filter((s) => s.selected === true);
+  const representation =
+    selected.length === 0
+      ? 'select'
+      : selected.length === stocks.length
+      ? 'all'
+      : `${selected[0].tic}...`;
+
+  //   console.log('대표', representation);
+  //# all checkbox는 전체 stock이 선택되지 않으면 자동으로 풀림.
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <button
+          className='after:content-["▼"] after:relative after:left-1'
+          onClick={() => {
+            useShowing(!showing);
+          }}
+        >
+          {representation}
+        </button>
+
+        <ul
+          id="selection"
+          className="m-0 absolute top-6 bg-red-200 w-72 flex flex-col items-center justify-between overflow-hidden"
+          style={
+            !showing
+              ? { height: '0px' }
+              : {
+                  height: '100px',
+                }
+          }
+        >
+          <li className=" bg-red-300 w-full">
+            <input
+              type="checkbox"
+              id="all"
+              name="all"
+              readOnly
+              onClick={() => {
+                onStocks(undefined, true, allChecked);
+              }}
+              checked={allChecked}
+            />
+            <label htmlFor="all">All</label>
+          </li>
+          {stocks.map((stock) => (
+            <Stock key={stock.tic} stock={stock} onChecked={onStocks} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
