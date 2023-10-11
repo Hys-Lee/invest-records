ac7b60cf8945db86bc5e7f906d9bb1e74653bb77
Yunsoo <yunsoojoh@gmail.com>
2023-10-11 22:47:00 +0900

    Feat: fix trading detail history page

--- src/api/Stock/index.tsx
+++ src/api/Stock/index.tsx
@@ -1,9 +1,13 @@
-const Stock = () => {
+interface StockProps {
+  ticker: String;
+}
+
+const Stock = ({ ticker }: StockProps) => {
   return (
     <iframe
       width="98%"
       height="400"
-      src="https://widget.finnhub.io/widgets/stocks/chart?symbol=AAPL&amp;watermarkColor=%231db954&amp;backgroundColor=%23222222&amp;textColor=white"
+      src={`https://widget.finnhub.io/widgets/stocks/chart?symbol=${ticker}&amp;watermarkColor=%231db954&amp;backgroundColor=%23222222&amp;textColor=white`}
     ></iframe>
   );
 };
--- src/pages/TradingDetail.tsx
+++ src/pages/TradingDetail.tsx
@@ -1,10 +1,15 @@
-import user from '../user.json';
 import { useParams } from 'react-router-dom';
+import user from '../user.json';
+import Stock from '../api/Stock';
 
 export default function TradingDetail() {
   let { userId, ticker } = useParams();
 
   const id = typeof userId === 'string' && !isNaN(userId) && parseInt(userId);
+
+  if (!id) {
+    return <div>no data to show</div>;
+  }
 
   const trading = user.stockList.filter((x) => x.userId === id && x.ticker === ticker);
   trading.sort(function (a, b) {
@@ -12,23 +17,26 @@ export default function TradingDetail() {
   });
 
   return (
-    <>
-      <div className="flex flex-col justify-center text-left">
+    <div className="m-2">
+      <p className="pb-2 text-neutral-500">Trading History</p>
+      <div className="grid gap-4 justify-center text-left">
         {trading.map((x) => {
           return (
-            <div className="p-4 rounded-lg bg-neutral-200">
-              <div>date of trading</div>
+            <div className="p-4 w-[280px] rounded-lg bg-neutral-200">
+              <div className="font-bold">Date of Trading</div>
               <div>{x.date}</div>
-              <div>type of trading</div>
+              <div className="font-bold">Type of Trading</div>
               <div>{x.buyAndSell === 'b' ? 'buy' : 'sell'}</div>
-              <div>exchange rate of the day</div>
+              <div className="font-bold">Exchange Rate of the Day</div>
               <div>{x.exchangeRate}</div>
-              <div>trading amount</div>
+              <div className="font-bold">Trading Amount</div>
               <div>{x.quantity}</div>
             </div>
           );
         })}
       </div>
-    </>
+      <p className="pb-2 text-neutral-500">Realtime Stock Candles</p>
+      <Stock ticker={ticker} />
+    </div>
   );
 }
