2977e7f497f0bc8bc545785aa106589ad9eb6b50
Yunsoo <yunsoojoh@gmail.com>
2023-10-11 22:46:01 +0900

    Design: unify page style

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
 // import { useState } from 'react';
-import Stock from './api/Stock';
-import Exchange from './api/Exchange';
-import MainSummary from './components/MainSummary.tsx';
-import { Link } from 'react-router-dom';
+import Main from './pages/Main';
 
 function App() {
-  return (
-    <>
-      <div id="main" className="bg-neutral-700">
-        <div className="flex h-screen justify-center">
-          <div className="hidden sm:flex sm:items-center mr-4 w-[375px]">
-            <div className="text-white font-bold text-center">
-              <h2 className="text-4xl">
-                Am I in <span className="text-red-600">the red</span> <br /> or{' '}
-                <span className="text-green-600">the green</span> ?
-              </h2>
-              <h3 className="pt-8 text-xl">
-                Get instant answers <br />
-                about your profits and losses.
-              </h3>
-            </div>
-          </div>
-          <div className="flex flex-col items-center py-[60px] rounded-3xl  w-[375px] bg-white text-left">
-            <p className="pl-4 font-bold text-[28px]">Stock Contents</p>
-            <div className=" rounded-md py-4 w-full px-1 h-full bg-[#F2F2F2]">
-              <div className=" flex flex-col items-center px-0">
-                <Link to={'/detail/'}>
-                  <MainSummary />
-                </Link>
-                <p className="pb-2 text-neutral-500">Realtime Stock Candles</p>
-                <Stock />
-                <p className="pt-8 pb-2 text-neutral-500">Realtime Exchange Rate</p>
-                <Exchange />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <Main />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -24,6 +24,29 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <>
+      <div id="main" className="bg-neutral-700">
+        <div className="flex h-screen justify-center">
+          <div className="hidden sm:flex sm:items-center mr-4 w-[375px]">
+            <div className="text-white font-bold text-center">
+              <h2 className="text-4xl">
+                Am I in <span className="text-red-600">the red</span> <br /> or{' '}
+                <span className="text-green-600">the green</span> ?
+              </h2>
+              <h3 className="pt-8 text-xl">
+                Get instant answers <br />
+                about your profits and losses.
+              </h3>
+            </div>
+          </div>
+          <div className="flex flex-col items-center py-[60px] rounded-3xl  w-[375px] bg-white text-left">
+            <p className="pl-4 font-bold text-[28px]">Stock Contents</p>
+            <div className="rounded-md py-4 w-full px-1 h-full bg-[#F2F2F2] overflow-auto">
+              <RouterProvider router={router} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   </React.StrictMode>,
 );
