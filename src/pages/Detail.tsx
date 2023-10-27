f52420b8330fde935e1cdef821eb2b0fbc41ebd8
Hys-Lee <dltkdgus3275@gmail.com>
2023-10-28 04:02:25 +0900

    Feat: Designed all buttons
    middle size for add button, small size for edit button

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -13,28 +13,30 @@ export default function Detail() {
   }
   return (
     <>
-      <div className=" h-full  m-2 w-72 flex flex-col items-center">
-        <Link to={`/`} className=" text-center">
+      <div className=" h-full  m-2 w-72">
+        <Link to={`/`} className=" text-left">
           <span className="  text-2xl font-extrabold">&lt;</span>
         </Link>
-        <h2 className=" mb-5 text-center text-3xl font-bold">Tickers</h2>
-        <div className=" pt-3 flex flex-col  items-center h-full gap-3 overflow-scroll">
-          {stockInfo.allTickers.map((ticker) => {
-            return (
-              <Link to={`/detail/${userId}/${ticker}`}>
-                <StockTile
-                  title={ticker}
-                  currentSaving={stockInfo.currentWon([ticker])}
-                  recentDate={tmpRecentStockDate}
-                  onAndOff={tmpRecentStockDate === new Date().toLocaleDateString()}
-                  profitAndLossPercent={stockInfo.earningRateWon([ticker])}
-                />
-              </Link>
-            );
-          })}
-        </div>
-        <div>
-          <Button />
+        <div className=" h-full pb-10 flex flex-col items-center">
+          <h2 className=" mb-5 text-center text-3xl font-bold">Tickers</h2>
+          <div className=" pt-3 flex flex-col  items-center h-full gap-3 overflow-scroll">
+            {stockInfo.allTickers.map((ticker) => {
+              return (
+                <Link to={`/detail/${userId}/${ticker}`}>
+                  <StockTile
+                    title={ticker}
+                    currentSaving={stockInfo.currentWon([ticker])}
+                    recentDate={tmpRecentStockDate}
+                    onAndOff={tmpRecentStockDate === new Date().toLocaleDateString()}
+                    profitAndLossPercent={stockInfo.earningRateWon([ticker])}
+                  />
+                </Link>
+              );
+            })}
+          </div>
+          <div>
+            <Button userId={userId} content="Add" w="md" />
+          </div>
         </div>
 
         {/* <Exchange /> */}
--- src/pages/TradingDetail.tsx
+++ src/pages/TradingDetail.tsx
@@ -1,6 +1,7 @@
 import { useParams, Link } from 'react-router-dom';
 import user from '../user.json';
 import Stock from '../api/Stock';
+import Button from '../components/Button';
 
 export default function TradingDetail() {
   let { userId, ticker } = useParams() as { userId: string; ticker: string };
@@ -18,9 +19,13 @@ export default function TradingDetail() {
 
   return (
     <div className="m-2 w-72  ">
-      <Link to={`/detail/`} className=" text-center">
-        <span className="  text-2xl font-extrabold">&lt;</span>
-      </Link>
+      <div className="flex flex-row justify-between items-center">
+        <Link to={`/detail/`} className=" text-center">
+          <span className="  text-2xl font-extrabold">&lt;</span>
+        </Link>
+        <Button userId={id} content="Edit" w="sm" />
+      </div>
+
       <h2 className=" text-center text-3xl font-bold">{ticker}</h2>
       <p className="pb-2 text-neutral-500">Trading History</p>
       <div className="grid gap-4 justify-center text-left overflow-scroll h-60">
